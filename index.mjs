ee92f832010dcadfc7023747891f71d4616ae258
Jordan Harband <ljharb@gmail.com>
2024-09-17 16:20:58 -0400

    [patch] remove useless ESM files

--- index.mjs
+++
@@ -1,7 +0,0 @@
-import getPolyfill from 'es-map/polyfill';
-
-export default getPolyfill();
-
-export { default as getPolyfill } from 'es-map/polyfill';
-export { default as implementation } from 'es-map/implementation';
-export { default as shim } from 'es-map/shim';
